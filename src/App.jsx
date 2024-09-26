4efed20e36d49d757fae608f60124cfef6159e36
rmalldt <rmall.dt@gmail.com>
2024-09-26 16:01:59 +0100

    Added styled-components dependency and created GlobalStyles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,38 @@
+import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+
+const StyledApp = styled.div`
+  height: 100vh;
+  background-color: beige;
+  padding: 0.8rem 1.2rem;
+`;
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: aquamarine;
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem1.2rem;
+  padding: 10px;
+`;
+
 function App() {
-  return <div>%BnBBackOffice%</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>BnBBackOffice</H1>
+        <Button onClick={() => alert('Playing')}>Play</Button>
+        <Button onClick={() => alert('Stop')}>Stop</Button>
+        <Input type="text" placeholder="Type something" />
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
