611656fbfc6d5c8aadc2f37ad170b3c34268abf5
rmalldt <rmall.dt@gmail.com>
2024-09-27 19:46:25 +0100

    Added react-query and proided to the App component. Added reusable components Form, FileInputs and Input. Modified Cabins page

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,26 +1,17 @@
-import { useEffect, useState } from 'react';
+import CabinTable from '../features/cabins/CabinTable';
 import Heading from '../ui/Heading';
 import Row from '../ui/Row';
-import { getCabins } from '../services/apiCabins';
 
 function Cabins() {
-  const [cabins, setCabins] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await getCabins();
-      setCabins(data);
-    };
-    fetchData();
-  }, []);
-
-  console.log(cabins);
-
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+      </Row>
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
