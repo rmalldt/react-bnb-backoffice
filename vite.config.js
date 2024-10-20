f4288b76709720570132b618b2ca3bf438edb5b7
rmalldt <rmall.dt@gmail.com>
2024-10-20 23:35:51 +0100

    Defined env variables in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    'process.env.VITE_SUPABASE_URL': JSON.stringify(
+      process.env.VITE_SUPABASE_URL
+    ),
+    'process.env.VITE_SUPABASE_STORAGE_URL': JSON.stringify(
+      process.env.VITE_SUPABASE_STORAGE_URL
+    ),
+    'process.env.VITE_SUPABASE_KEY': JSON.stringify(
+      process.env.VITE_SUPABASE_KEY
+    ),
+  },
+});
